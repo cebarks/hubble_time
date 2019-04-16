4430e4e1ef62ddc28c01a76934be0bf0accaf0c8
Anten Skrabec <cebarks@gmail.com>
2019-04-16 15:52:03 -0600

    implements favorites only view

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,12 +1,12 @@
 class Image
-  attr_accessor :id, :name, :description, :credits, :image_files
-  
+  attr_accessor :hubble_id, :name, :description, :credits, :file_url
+
   def initialize(args)
-    @id = args[:id]
+    @hubble_id = args[:id]
     @name = args[:name]
     @description = args[:description]
     @credits = args[:credits]
-    @image_files = args[:image_files]
+    @file_url = args[:image_files].first[:file_url]
   end
 
   def self.all
